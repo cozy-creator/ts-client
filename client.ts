84f0f0801c65b5410df97f9d01951aaa094b4019
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-10-26 14:08:25 +0100

    Refactor CozyCreator to use private class fields and provide public getters for file, workflow, and generation APIs

--- client.ts
+++ client.ts
@@ -2,15 +2,27 @@ import * as runtime from "./runtime";
 import { FileApi, GenerationApi, WorkflowApi } from "./apis";
 
 export class CozyCreator extends runtime.BaseAPI {
-  public file: FileApi;
-  public workflow: WorkflowApi;
-  public generation: GenerationApi;
+  #fileApi: FileApi;
+  #workflowApi: WorkflowApi;
+  #generationApi: GenerationApi;
 
   constructor(configuration?: runtime.Configuration) {
     super(configuration);
 
-    this.file = new FileApi(this.configuration);
-    this.workflow = new WorkflowApi(this.configuration);
-    this.generation = new GenerationApi(this.configuration);
+    this.#fileApi = new FileApi(this.configuration);
+    this.#workflowApi = new WorkflowApi(this.configuration);
+    this.#generationApi = new GenerationApi(this.configuration);
+  }
+
+  public get file() {
+    return this.#file;
+  }
+
+  public get workflow() {
+    return this.#workflow;
+  }
+
+  public get generation() {
+    return this.#generation;
   }
 }
